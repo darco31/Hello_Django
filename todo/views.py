e5adf09950b819585ac75d7c792aa51bde81b2e7
Stephen <sdarcy29@hotmail.com>
2022-03-24 20:46:06 +0000

    Add a new view for add item, use is tatement to check if it a POST request, if so post a new todo if not redirect to home page

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -9,3 +9,15 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, "todo/todo_list.html", context)
+
+
+# The below if statement checks if the method is a POST request, if it is the
+# info from the form will create a new item.
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, "todo/add_item.html")
